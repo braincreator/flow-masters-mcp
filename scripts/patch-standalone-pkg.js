3d0de4afa9a156131d6b93afb757d75017803182
Aleksandr Yudin <alexxudin@gmail.com>
2025-07-05 23:49:46 +0300

    .

--- scripts/patch-standalone-pkg.js
+++ scripts/patch-standalone-pkg.js
@@ -1,23 +1,23 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
-const packageJsonPath = path.join(process.cwd(), '.next', 'standalone', 'package.json');
+const packageJsonPath = path.join(process.cwd(), '.next', 'standalone', 'package.json')
 
 fs.readFile(packageJsonPath, 'utf8', (err, data) => {
   if (err) {
-    console.error('Error reading package.json:', err);
-    return;
+    console.error('Error reading package.json:', err)
+    return
   }
 
-  const packageJson = JSON.parse(data);
+  const packageJson = JSON.parse(data)
 
-  packageJson.type = 'module';
+  packageJson.type = 'commonjs'
 
   fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8', (err) => {
     if (err) {
-      console.error('Error writing package.json:', err);
-      return;
+      console.error('Error writing package.json:', err)
+      return
     }
-    console.log('Successfully patched .next/standalone/package.json');
-  });
-});
+    console.log('Successfully patched .next/standalone/package.json to use CommonJS')
+  })
+})
