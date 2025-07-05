a0c3a229cd3a3e090f95c031a01ae9a55a42e6c6
Aleksandr Yudin <alexxudin@gmail.com>
2025-07-05 23:10:20 +0300

    .

---
+++ scripts/patch-standalone-pkg.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const packageJsonPath = path.join(process.cwd(), '.next', 'standalone', 'package.json');
+
+fs.readFile(packageJsonPath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading package.json:', err);
+    return;
+  }
+
+  const packageJson = JSON.parse(data);
+  packageJson.type = 'commonjs';
+
+  fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing package.json:', err);
+      return;
+    }
+    console.log('Successfully patched .next/standalone/package.json');
+  });
+});
